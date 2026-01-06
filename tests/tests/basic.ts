59eb0a06e9f4cc3ea3cacbf6e21c4d13eedba7ac
Carifio24 <carifio24@gmail.com>
2026-01-06 12:37:20 -0700

    Allow awaiting our expect utilities.

--- tests/tests/basic.ts
+++ tests/tests/basic.ts
@@ -40,23 +40,23 @@ const tests: CarinaTests = {
   },
 
   'Initial configuration': async function() {
-    app.expect.title().to.equal(this.app.props.title);
-    expectAllVisible(this.app, [
+    await app.expect.title().to.equal(this.app.props.title);
+    await expectAllVisible(this.app, [
       "@splashScreen",
       "@splashClose",
     ]);
-    expectAllNotPresent(this.app, [
+    await expectAllNotPresent(this.app, [
       "@videoDialog",
       "@infoSheet"
     ]);
 
-    this.app.click("@splashClose");
-    expectAllNotPresent(this.app, [
+    await this.app.click("@splashClose");
+    await expectAllNotPresent(this.app, [
       "@splashScreen",
       "@splashClose"
     ]);
 
-    expectAllVisible(this.sections.topContent, [
+    await expectAllVisible(this.sections.topContent, [
       "@videoIcon",
       "@showHideButton",
       "@resetIcon",
@@ -64,7 +64,7 @@ const tests: CarinaTests = {
     ]);
 
     const bottomContent = this.sections.bottomContent;
-    expectAllVisible(bottomContent, [
+    await expectAllVisible(bottomContent, [
       "@tools",
       "@hubbleButton",
       "@jwstButton",
@@ -72,38 +72,38 @@ const tests: CarinaTests = {
       "@credits",
     ]);
 
-    bottomContent.expect.elements("@creditIcon").count.to.equal(bottomContent.props.creditIconCount);
+    await bottomContent.expect.elements("@creditIcon").count.to.equal(bottomContent.props.creditIconCount);
 
-    this.app.waitForElementVisible("@userExperience", 30_000);
-    this.sections.userExperience.click("@closeButton");
-    this.app.expect.element("@userExperience").to.not.be.present;
+    await this.app.waitForElementVisible("@userExperience", 30_000);
+    await this.sections.userExperience.click("@closeButton");
+    await this.app.expect.element("@userExperience").to.not.be.present;
 
     await percyScreenshot(this.driver, "Initial");
   },
 
   'Layer buttons': async function() {
     const bottomContent = this.sections.bottomContent;
-    bottomContent.click("@hubbleButton");
-    bottomContent.expect.element("@slider").value.to.equal("0");
+    await bottomContent.click("@hubbleButton");
+    await bottomContent.expect.element("@slider").value.to.equal("0");
 
-    bottomContent.click("@jwstButton");
-    bottomContent.expect.element("@slider").value.to.equal("100");
+    await bottomContent.click("@jwstButton");
+    await bottomContent.expect.element("@slider").value.to.equal("100");
 
     const topContent = this.sections.topContent;
-    topContent.click("@showHideButton");
+    await topContent.click("@showHideButton");
 
-    await percyScreenshot(this.driver, "Images hidden");
-    
-    expectAllNotPresent(bottomContent, [
+    await expectAllNotPresent(bottomContent, [
       "@tools",
       "@hubbleButton",
       "@jwstButton",
       "@slider"
     ]);
-    bottomContent.expect.element("@credits").to.be.visible;
+    await bottomContent.expect.element("@credits").to.be.visible;
 
-    topContent.click("@showHideButton");
-    expectAllVisible(bottomContent, [
+    await percyScreenshot(this.driver, "Images hidden");
+
+    await topContent.click("@showHideButton");
+    await expectAllVisible(bottomContent, [
       "@tools",
       "@hubbleButton",
       "@jwstButton",
@@ -111,43 +111,44 @@ const tests: CarinaTests = {
       "@credits"
     ]);
 
-
     await percyScreenshot(this.driver, "Images shown");
   },
 
   'Open video': async function() {
-    this.sections.topContent.click("@videoIcon");
-    this.app.expect.element("@videoDialog").to.be.visible;
-    expectAllVisible(this.sections.videoDialog, [
+    await this.sections.topContent.click("@videoIcon");
+    await this.app.expect.element("@videoDialog").to.be.visible;
+    await expectAllVisible(this.sections.videoDialog, [
       "@video", "@closeIcon",
     ]);
 
     await percyScreenshot(this.driver, "Video open");
 
-    this.sections.videoDialog.click("@closeIcon");
-    this.app.expect.element("@videoDialog").to.not.be.present;
+    await this.sections.videoDialog.click("@closeIcon");
+    await this.app.expect.element("@videoDialog").to.not.be.present;
+
+    await percyScreenshot(this.driver, "Video closed");
   },
 
   'Info text': async function(browser: NightwatchBrowser) {
-    this.sections.topContent.click("@textIcon");
-    this.app.expect.element("@infoSheet").to.be.visible;
+    await this.sections.topContent.click("@textIcon");
+    await this.app.expect.element("@infoSheet").to.be.visible;
 
     const infoSheet = this.sections.infoSheet;
-    expectAllVisible(infoSheet, [
+    await expectAllVisible(infoSheet, [
       "@closeIcon",
       "@infoTabHeader",
       "@wwtTabHeader",
       "@infoText"
     ]);
-    infoSheet.expect.element("@wwtText").to.not.be.present;
-    infoSheet.expect.elements("@tabHeader").count.to.equal(infoSheet.props.tabCount);
+    await infoSheet.expect.element("@wwtText").to.not.be.present;
+    await infoSheet.expect.elements("@tabHeader").count.to.equal(infoSheet.props.tabCount);
 
     await percyScreenshot(this.driver, "Info sheet open");
     
     // getWindowSize seems to return the height of the browser
     // (that is, including the tab and address bars)
     // but we want just the height of the viewport
-    this.app.getElementSize("html", (windowSize) => {
+    await this.app.getElementSize("html", (windowSize) => {
       this.app.getElementSize("@mainContent", (mainContentSize) => {
         const wsize = windowSize.value as WindowSize;
         const csize = mainContentSize.value as WindowSize;
@@ -156,15 +157,15 @@ const tests: CarinaTests = {
       });
     });
 
-    infoSheet.click("@wwtTabHeader");
-    infoSheet.expect.element("@infoText").to.be.present;
-    infoSheet.expect.element("@infoText").to.not.be.visible;
+    await infoSheet.click("@wwtTabHeader");
+    await infoSheet.expect.element("@infoText").to.be.present;
+    await infoSheet.expect.element("@infoText").to.not.be.visible;
 
     await percyScreenshot(this.driver, "Info sheet - WWT Tab");
 
-    infoSheet.click("@infoTabHeader");
-    infoSheet.expect.element("@wwtText").to.be.present;
-    infoSheet.expect.element("@wwtText").to.not.be.visible;
+    await infoSheet.click("@infoTabHeader");
+    await infoSheet.expect.element("@wwtText").to.be.present;
+    await infoSheet.expect.element("@wwtText").to.not.be.visible;
 
     await percyScreenshot(this.driver, "Info sheet - Info Tab");
   },
